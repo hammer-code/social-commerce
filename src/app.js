0158299164b605f7487e537503eadeb12cfbab85
eezhal92 <arkinthesky.69@hotmail.com>
2017-10-30 09:53:55 +0800

    add detail product page

--- src/app.js
+++ src/app.js
@@ -6,18 +6,45 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+// Routing
+
+/**
+ * Lihat daftar produk
+ */
 app.get('/', function (req, res) {
   database.Product.findAll()
     .then(function (products) {
-      res.render('product-list', { title: 'Daftar Produk', products: products });
+      res.render('product-list', { products: products });
     })
     .catch(function (error) {
       res.send(error);
     });
 });
 
+/**
+ * Lihat halaman buat produk
+ */
 app.get('/create', function (req, res) {
-  res.render('product-create', { title: 'Buat Postingan' });
+  res.render('product-create');
+});
+
+/**
+ * Lihat detail produk
+ */
+app.get('/:id', function (req, res) {
+  database.Product.findById(req.params.id)
+    .then(function (product) {
+      if (!product) {
+        res.render('not-found');
+
+        return;
+      }
+
+      res.render('product-detail', { product: product });
+    })
+    .catch(function (error) {
+      res.send(error);
+    });
 });
 
 module.exports = app;
